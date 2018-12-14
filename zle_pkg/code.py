eec18fa8d4a03f003f670555b6e796a461910bb7
zach.escalante <zach.escalante@gmail.com>
2018-12-14 14:38:54 -0500

    Adding the code.py file as a test to see if the functionality can be imported

---
+++ zle_pkg/code.py
@@ -0,0 +1,14 @@
+import os
+import regex
+import numpy as np
+import pandas as pd
+
+
+class StringFormatting():
+
+	def __init__(self):
+
+		print('StringFormatting() package has been installed')
+
+	def lower_case(word):
+		return word.lower()
