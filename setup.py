bcc69232c3bfc33ff902a80b30434101314a84be
zach.escalante <zach.escalante@gmail.com>
2019-01-25 15:22:10 -0500

    implementing @classmethod variable v.1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zle_pkg",
-    version="0.1.9",
+    version="0.1.10",
     author="Zachary Escalante",
     author_email="zach.escalante@gmail.com",
     description="NLP string formatting package",
